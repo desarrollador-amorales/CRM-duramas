010ec6b4f116c52bf328cb6e0c99617836e5c18f
amorales <desarrollador@duramas.com.ec>
2021-04-30 12:54:22 -0500

    Se crea el grafico de viistas diarias por asesor en la administracion, cambios a español

--- leftbar.php
+++ leftbar.php
@@ -1,33 +1,35 @@
  <!-- BEGIN SIDEBAR -->
-  <div class="page-sidebar" id="main-menu">
-    <!-- BEGIN MINI-PROFILE -->
-    <div class="page-sidebar-wrapper scrollbar-dynamic" id="main-menu-wrapper">
-      <div class="user-info-wrapper">
-        <div class="profile-wrapper"> <img src="assets/img/user.png"  alt="" data-src="assets/img/user.png" data-src-retina="assets/img/user.png" width="69" height="69" /> </div>
-        <div class="user-info">
-          <div class="greeting" style="font-size:14px;">Bienvenido</div>
-          <div class="username" style="font-size:12px;"><?php echo $_SESSION['name'];?></div>
-          <div class="status" style="font-size:10px;"><a href="#">
-            <div class="status-icon green"></div>
-            Contectado</a></div>
-        </div>
-      </div>
-      <!-- END MINI-PROFILE -->
-      <!-- BEGIN SIDEBAR MENU -->
-      <p class="menu-title"><span class="pull-right"><a href="javascript:;" class="reload"><i class=""></i></a></span></p>
-   
-    <ul>	
-      <li class="start"> <a href="dashboard.php"> <i class="icon-custom-home"></i> <span class="title">Escritorio</span> <span class="selected"></span>  </a> 
-		    </li>
-    
-          <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Cambiar Contraseña</a></li>
-                            <li><a href="profile.php"><span class="fa fa-user"></span> Profile</a></li>
-                         
-                            
-                              <li ><a href="get-quote.php"> <span class="fa fa-tasks"></span> Request a Quote</a></li>
-							     <li ><a href="create-ticket.php"><span class="fa fa-ticket"></span> Create Ticket</a></li>
-                                <li ><a href="view-tickets.php"><span class="fa fa-ticket"></span> View Ticket</a></li>
-                           
-    </ul>
-    
-	
+ <div class="page-sidebar" id="main-menu">
+     <!-- BEGIN MINI-PROFILE -->
+     <div class="page-sidebar-wrapper scrollbar-dynamic" id="main-menu-wrapper">
+         <div class="user-info-wrapper">
+             <div class="profile-wrapper"> <img src="assets/img/user.png" alt="" data-src="assets/img/user.png"
+                     data-src-retina="assets/img/user.png" width="69" height="69" /> </div>
+             <div class="user-info">
+                 <div class="greeting" style="font-size:14px;">Bienvenido</div>
+                 <div class="username" style="font-size:12px;"><?php echo $_SESSION['name'];?></div>
+                 <div class="status" style="font-size:10px;"><a href="#">
+                         <div class="status-icon green"></div>
+                         Contectado
+                     </a></div>
+             </div>
+         </div>
+         <!-- END MINI-PROFILE -->
+         <!-- BEGIN SIDEBAR MENU -->
+         <p class="menu-title"><span class="pull-right"><a href="javascript:;" class="reload"><i
+                         class=""></i></a></span></p>
+
+         <ul>
+             <li class="start"> <a href="dashboard.php"> <i class="icon-custom-home"></i> <span
+                         class="title">Escritorio</span> <span class="selected"></span> </a>
+             </li>
+
+             <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Cambiar Contraseña</a></li>
+             <li><a href="profile.php"><span class="fa fa-user"></span> Perfil</a></li>
+
+
+             <li><a href="get-quote.php"> <span class="fa fa-tasks"></span> Request a Quote</a></li>
+             <li><a href="create-ticket.php"><span class="fa fa-ticket"></span> Create Ticket</a></li>
+             <li><a href="view-tickets.php"><span class="fa fa-ticket"></span> View Ticket</a></li>
+
+         </ul>
--- login.php
+++ login.php
@@ -45,81 +45,89 @@ exit();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<meta charset="utf-8" />
-<title>CRM | Login</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<meta content="" name="description" />
-<meta content="" name="author" />
-<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta charset="utf-8" />
+    <title>CRM | Acceder</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css" />
 
 </head>
-<body class="error-body no-top">
-<div class="container">
-  <div class="row login-container column-seperation">  
-        <div class="col-md-5 col-md-offset-1">
-          <h2>Acceder a CRM Duramas</h2>
-          <p>
-            <a href="registration.php">Registrate Ahora!</a> ...</p>
-          <br>
 
-		   
+<body class="error-body no-top">
+    <div class="container">
+        <div class="row login-container column-seperation">
+            <div class="col-md-5 col-md-offset-1">
+                <h2>Acceder a CRM Duramas</h2>
+                <p>
+                    <a href="registration.php">Registrate Ahora!</a> ...
+                </p>
+                <br>
+
+
+            </div>
+            <div class="col-md-5 "> <br>
+                <p style="color:#F00"><?php echo $_SESSION['action1'];?><?php echo $_SESSION['action1']="";?></p>
+                <form id="login-form" class="login-form" action="" method="post">
+                    <div class="row">
+                        <div class="form-group col-md-10">
+                            <label class="form-label">Email</label>
+                            <div class="controls">
+                                <div class="input-with-icon  right">
+                                    <i class=""></i>
+                                    <input type="email" name="email" id="txtusername" class="form-control"
+                                        required="true">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-10">
+                            <label class="form-label">Contraseña</label>
+                            <span class="help"></span>
+                            <div class="controls">
+                                <div class="input-with-icon  right">
+                                    <i class=""></i>
+                                    <input type="password" name="password" id="txtpassword" class="form-control"
+                                        required="true">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="control-group  col-md-10">
+                            <div class="checkbox checkbox check-success"> <a href="forgot-password.php">Olvido su
+                                    Contraseña</a>&nbsp;&nbsp;
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-10">
+                            <button class="btn btn-primary btn-cons pull-right" name="login"
+                                type="submit">Acceder</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+
         </div>
-        <div class="col-md-5 "> <br>
-             <p style="color:#F00"><?php echo $_SESSION['action1'];?><?php echo $_SESSION['action1']="";?></p>
-		 <form id="login-form" class="login-form" action="" method="post">
-		 <div class="row">
-		 <div class="form-group col-md-10">
-            <label class="form-label">Email</label>
-            <div class="controls">
-				<div class="input-with-icon  right">                                        
-					<i class=""></i>
-					<input type="email" name="email" id="txtusername" class="form-control" required="true">                                 
-				</div>
-            </div>
-          </div>
-          </div>
-		  <div class="row"> 
-          <div class="form-group col-md-10">
-            <label class="form-label">Contraseña</label>
-            <span class="help"></span>
-            <div class="controls">
-				<div class="input-with-icon  right">                                       
-					<i class=""></i>
-					<input type="password" name="password" id="txtpassword" class="form-control" required="true">                                 
-				</div>
-            </div>
-          </div>
-          </div>
-		  <div class="row">
-          <div class="control-group  col-md-10">
-            <div class="checkbox checkbox check-success"> <a href="forgot-password.php">Olvido su Contraseña</a>&nbsp;&nbsp;
-         </div>
-          </div>
-          </div>
-          <div class="row">
-            <div class="col-md-10">
-              <button class="btn btn-primary btn-cons pull-right" name="login" type="submit">Login</button>
-            </div>
-          </div>
-		  </form>
-        </div>
-     
-    
-  </div>
-</div>
-<script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-<script src="assets/js/login.js" type="text/javascript"></script>
+    </div>
+    <script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="assets/js/login.js" type="text/javascript"></script>
 </body>
+
 </html>
